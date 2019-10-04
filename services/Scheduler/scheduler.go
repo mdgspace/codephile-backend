5d0d22011c3f6331323f37b118fff94737482aea
Arnesh07 <arnesh07@gmail.com>
2019-10-04 16:36:12 +0530

    Adds interface for the scheduler

---
+++ services/Scheduler/scheduler.go
@@ -0,0 +1,19 @@
+package scheduler
+
+import (
+	"github.com/jasonlvhit/gocron"
+)
+
+func task() {
+     //Updates profiles and submissions
+}
+
+func taskWithParams(a int, b string) {
+     //
+}
+
+func StartScheduling() {
+	// gocron.Every(5).Seconds().Do(task)
+	gocron.Every(1).Day().Do(task)   //task to be done everyday
+	<- gocron.Start()
+}
