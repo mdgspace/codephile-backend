0d7b11632d7f02f0b77d32645826419a8aa66624
Ritik kumar <ritikkne@gmail.com>
2019-10-04 21:53:27 +0530

    Revert "Adds interface for the scheduler (#36)" (#38)
    This reverts commit c9f51a6679ad65199829e616897201af231a6c87.

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import (
 	_ "github.com/mdg-iitr/Codephile/conf"
 	"github.com/astaxie/beego"
 	_ "github.com/mdg-iitr/Codephile/routers"
-	"github.com/mdg-iitr/Codephile/services/Scheduler"
 )
 
 func main() {
-	scheduler.StartScheduling()
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/docs"] = "swagger"
