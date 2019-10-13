e440f9ed7d2decc5ae02d0be1c6428486ede6f6c
Arnesh07 <arnesh07@gmail.com>
2019-10-13 14:54:33 +0530

    Minor change

--- services/scheduler/scheduler.go
+++ services/scheduler/scheduler.go
@@ -7,7 +7,6 @@ import (
 
 func task() {
 	 //Updates profiles and submissions
-	 log.Println("HELLO")
 }
 
 func taskWithParams(a int, b string) {
@@ -15,7 +14,7 @@ func taskWithParams(a int, b string) {
 }
 
 func StartScheduling() {
-	gocron.Every(5).Seconds().Do(task)
+	// gocron.Every(5).Seconds().Do(task)
 	gocron.Every(1).Day().Do(task)   //task to be done everyday
 	<- gocron.Start()
 }
