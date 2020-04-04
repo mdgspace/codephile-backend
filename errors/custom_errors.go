af64e7d32ccbd70c1d565bec19c95b6536663acf
Prateek Sachan <psachan@cs.iitr.ac.in>
2020-04-04 17:59:57 +0530

    Added worker triggered by endpoint request

--- errors/custom_errors.go
+++ errors/custom_errors.go
@@ -9,3 +9,5 @@ var UserAlreadyExistError = errors.New("could not create user: Username already 
 var UserNotFoundError = errors.New("user not found")
 
 var HandleNotFoundError = errors.New("handle not available")
+
+var ErrJobQueueFull = errors.New("Job queue completely full")
