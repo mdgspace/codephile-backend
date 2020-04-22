74a7b82fb1ddf23896785db51094ac37145f16a8
Prateek Sachan <psachan@cs.iitr.ac.in>
2020-04-22 17:40:54 +0530

    Add option to provide app root through env variable

--- conf/load_env.go
+++ conf/load_env.go
@@ -4,6 +4,7 @@ import (
 	"github.com/joho/godotenv"
 	search "github.com/mdg-iitr/Codephile/services/elastic"
 	"log"
+	"os"
 	"path/filepath"
 	"runtime"
 )
@@ -13,9 +14,14 @@ var AppRootDir string
 func init() {
 	search.GetElasticClient()
 	_, file, _, _ := runtime.Caller(0)
-	AppRootDir = filepath.Dir(filepath.Dir(file))
 	err := godotenv.Load(filepath.Join(filepath.Dir(file), ".env"))
 	if err != nil {
 		log.Println("No .env file found")
+	}
+	if root := os.Getenv("APP_ROOT"); root != "" {
+		AppRootDir = root
+	} else {
+		_, file, _, _ := runtime.Caller(0)
+		AppRootDir = filepath.Dir(filepath.Dir(file))
 	}
 }
