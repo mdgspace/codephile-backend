4e1baa4a6e87d4e042ab2c615f75e49f0d948335
Prateek Sachan <psachan@cs.iitr.ac.in>
2020-05-01 11:42:14 +0530

    Add full name and picture to feed

--- models/types/feed.go
+++ models/types/feed.go
@@ -5,5 +5,7 @@ import "github.com/globalsign/mgo/bson"
 type FeedObject struct {
 	UserName   string        `json:"username"`
 	ID         bson.ObjectId `json:"user_id" bson:"_id"`
+	FullName   string        `json:"fullname"`
+	Picture    string        `json:"picture"`
 	Submission Submission    `json:"submission"`
 }
