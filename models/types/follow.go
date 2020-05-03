2efacbe1c4d9d7274a9340bc856275ff5754fa89
Arnesh07 <arnesh07@gmail.com>
2020-05-03 23:31:57 +0530

    adds username and picture fields to follow/following endpoint

--- models/types/follow.go
+++ models/types/follow.go
@@ -7,21 +7,25 @@ import (
 	// "github.com/mdg-iitr/Codephile/models"
 )
 
-type Following struct{
-	CodephileHandle string          `bson:"codephile_handle" json:"codephile_handle"`
-	ID              bson.ObjectId   `bson:"f_id" json:"f_id"`
+type Following struct {
+	ID bson.ObjectId `bson:"f_id" json:"f_id"`
 }
 
-type WorldRankComparison struct{
-	WorldRank1   string             `bson:"rank1" json:"rank1"`
-	WorldRank2   string             `bson:"rank2" json:"rank2"`
-} 
+type FollowingUser struct {
+	ID       bson.ObjectId `bson:"_id" json:"_id"`
+	Username string        `bson:"username" json:"username" schema:"username"`
+	FullName string        `bson:"fullname" json:"fullname" schema:"fullname"`
+	Picture  string        `bson:"picture" json:"picture"`
+}
+
+type WorldRankComparison struct {
+	WorldRank1 string `bson:"rank1" json:"rank1"`
+	WorldRank2 string `bson:"rank2" json:"rank2"`
+}
 
 type AllWorldRanks struct {
-	CodechefWorldRanks      WorldRankComparison    `bson:"codechef_ranks" json:"codechef_ranks"`
-	CodeforcesWorldRanks    WorldRankComparison    `bson:"codeforces_ranks" json:"codeforces_ranks"`
-	HackerrankWorldRanks    WorldRankComparison    `bson:"hackerrank_ranks" json:"hackerrank_ranks"`
-	SpojWorldRanks          WorldRankComparison    `bson:"spoj_ranks" json:"spoj_ranks"`
+	CodechefWorldRanks   WorldRankComparison `bson:"codechef_ranks" json:"codechef_ranks"`
+	CodeforcesWorldRanks WorldRankComparison `bson:"codeforces_ranks" json:"codeforces_ranks"`
+	HackerrankWorldRanks WorldRankComparison `bson:"hackerrank_ranks" json:"hackerrank_ranks"`
+	SpojWorldRanks       WorldRankComparison `bson:"spoj_ranks" json:"spoj_ranks"`
 }
-
-
