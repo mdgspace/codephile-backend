19dfcb81ce5aa36314ece62e84b1cfc33fda41b8
Saksham-Sharma-99 <sakshamsharma99@outlook.com>
2021-04-16 03:53:09 +0530

    #INCOMPLETE , DONT MERGE RIGHT NOW have not written the part to refresh  access token yet. Need help with it

--- services/mail/mail_sender.go
+++ services/mail/mail_sender.go
@@ -1,28 +1,66 @@
 package mail
 
 import (
+	"context"
+	"encoding/base64"
 	"log"
-	"net/smtp"
 	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
 )
 
 //send a text mail to the receiver's email id
 func SendMail(to string, subject string, body string) {
-	// Sender data.
-	from := os.Getenv("EMAIL_SERVER_USER")
-	password := os.Getenv("EMAIL_SERVER_PASS")
-	smtpHost := os.Getenv("EMAIL_SMTP_HOST")
-	smtpPort := os.Getenv("EMAIL_SMTP_PORT")
+	var GmailService *gmail.Service
 
-	// Message.
-	msg := "From: Codephile" + "\n" + "To: " + to+ "\n" + "Subject: "+ subject + "\n\n" + body
-	// Authentication.
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	config := oauth2.Config{
+		ClientID:     os.Getenv("EMAIL_CLIENT"),
+		ClientSecret: os.Getenv("EMAIL_SECRET"),
+		Endpoint:     google.Endpoint,
+		RedirectURL:  "http://localhost:8080",
+	}
 
-	// Sending email.
-	err := smtp.SendMail(smtpHost + ":" + smtpPort, auth, from, []string{to}, []byte(msg))
+	token := oauth2.Token{
+		AccessToken:  os.Getenv("EMAIL_ACCESS_TOKEN"),
+		RefreshToken: os.Getenv("EMAIL_REFRESH_TOKEN"),
+		TokenType:    "Bearer",
+		Expiry:       time.Now(),
+	}
+
+	//need to write the part for token refresh ..
+
+	// ..
+
+	var tokenSource = config.TokenSource(context.Background(), &token)
+
+	srv, err := gmail.NewService(context.Background(), option.WithTokenSource(tokenSource))
 	if err != nil {
-		log.Println(err)
-		return
+		log.Printf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	GmailService = srv
+	if GmailService != nil {
+		log.Println("Email service is initialized \n")
+	}
+
+	var message gmail.Message
+
+	emailTo := "To: " + to + "\r\n"
+	sub := "Subject: " + subject + "\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
+	msg := []byte(emailTo + sub + mime + "\n" + body)
+
+	message.Raw = base64.URLEncoding.EncodeToString(msg)
+
+	_, err = GmailService.Users.Messages.Send("me", &message).Do()
+	if err != nil {
+		log.Fatalf(err.Error())
 	}
 }
+
+//EMAIL_SECRET=C65v5DEIHlDdEU1shHYrlnns
+//EMAIL_CLIENT=918242223374-k6ihlaib00kvt65j82gnqnrukms6ocrg.apps.googleusercontent.com
